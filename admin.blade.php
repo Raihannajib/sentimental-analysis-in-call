1d4894026b6ca6ea746627646cd5a75fb1507471
Najib Raihan <najibraihan3@gmail.com>
2021-09-24 19:04:34 +0200

    php files

---
+++ admin.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>laravel 7 file upload example - ItSolutionStuff.com.com</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+  
+<body>
+<div class="container">
+   
+    <div class="panel panel-primary">
+      <div class="panel-heading"><h2>Upload fichiers</h2></div>
+      <div class="panel-body">
+   
+        @if ($message = Session::get('success'))
+        <div class="alert alert-success alert-block">
+            <button type="button" class="close" data-dismiss="alert">×</button>
+                <strong>{{ $message }}</strong>
+        </div>
+        <!-- <img src="uploads/{{ Session::get('file') }}"> -->
+        @endif
+  
+        @if (count($errors) > 0)
+            <div class="alert alert-danger">
+                <strong>Whoops!</strong> probleme d'insertion .
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+  
+        <form action="{{ route('file.upload.post') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="row">
+  
+                <div class="col-md-6">
+                    <input type="file" name="file" class="form-control">
+                </div>
+   
+                <div class="col-md-6">
+                    <button type="submit" class="btn btn-success">Upload</button>
+                </div>
+   
+            </div>
+        </form>
+  
+      </div>
+    </div>
+</div>
+</body>
+  
+</html>
